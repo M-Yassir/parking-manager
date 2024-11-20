f58f82b0bcb88b73fe1a214701be91216dc30b08
Mohamed Yassir Abouchiba <m.abouchiba0@gmail.com>
2024-11-20 15:17:18 +0100

    Add files via upload

---
+++ Code/Car.java
@@ -0,0 +1,38 @@
+public class Car extends Vehicle {
+    private boolean HasFourDoors;
+
+    public Car() {
+        super();
+        this.HasFourDoors = false;
+    }
+
+    public Car(String name, String model, String registrationNumber, String type, String date, int price, boolean HasFourDoors) {
+        super(name, model, registrationNumber, type, date, price, 0, false, false);
+        this.HasFourDoors = HasFourDoors;
+    }
+
+    public boolean isHasFourDoors() {
+        return HasFourDoors;
+    }
+
+    public void setHasFourDoors(boolean HasFourDoors) {
+        this.HasFourDoors = HasFourDoors;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        System.out.println("Name: " + getName());
+        System.out.println("Model: " + getModel());
+        System.out.println("Registration Number: " + getRegistrationNumber());
+        System.out.println("Type: " + getType());
+        System.out.println("Manufacture Date: " + getDate());
+        System.out.println("Price: " + getPrice() + " DH");
+        System.out.println("Has Sunroof: " + (HasFourDoors ? "Yes" : "No"));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", HasFourDoors=" + HasFourDoors + "]";
+    }
+}
