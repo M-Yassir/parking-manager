f7b8985e0114cc1fbde63e259791dba5520080d4
Mohamed Yassir Abouchiba <m.abouchiba0@gmail.com>
2024-11-20 15:15:13 +0100

    Update Customer.java

--- Code/Customer.java
+++ Code/Customer.java
@@ -1,5 +1,51 @@
 public class Customer {
-    String FirstName;
-    String LastName;
-    String NID;
+    private String firstName;
+    private String lastName;
+    private String nid; // National ID
+
+    // Default Constructor
+    public Customer() {
+        this.firstName = "Unknown";
+        this.lastName = "Unknown";
+        this.nid = "Unknown";
+    }
+
+    // Parameterized Constructor
+    public Customer(String firstName, String lastName, String nid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nid = nid;
+    }
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNid() {
+        return nid;
+    }
+
+    public void setNid(String nid) {
+        this.nid = nid;
+    }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return "Customer [FirstName=" + firstName + ", LastName=" + lastName + ", NID=" + nid + "]";
+    }
+
 }
